48dcb67621e6024915b3743559233c0ca044a1a9
TANTRIINDRIANI <tantri2253@gmail.com>
2022-09-29 13:07:26 +0700

    tan1

---
+++ untirta1/fh/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def prodifh(request):
+    return render(request, 'indexfh.html')
+
---
+++ untirta1/profil/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def prodiprofil(request):
+    return render(request, 'indexprofil.html')
