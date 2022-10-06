605f74326fa8c217b89e9a75ee166ebc3115cd76
TANTRIINDRIANI <tantri2253@gmail.com>
2022-10-06 22:26:59 +0700

    database

--- untirta1/fkip/models.py
+++ untirta1/fkip/models.py
@@ -1,3 +1,41 @@
+from pickle import TRUE
 from django.db import models
 
 # Create your models here.
+class STAFF(models.Model):
+    NIP=models.CharField(max_length=50)
+    NAMA=models.CharField(max_length=40)
+    TTL=models.CharField(max_length=40)
+    PHOTO=models.CharField(max_length=40)
+    EMAIL=models.CharField(max_length=40)
+    UNIT=models.CharField(max_length=40)
+    ALAMAT=models.CharField(max_length=40)
+
+    def _str_(self):
+        return self.NIP
+
+class MAHASISWA(models.Model):
+    NIM=models.CharField(max_length=50)
+    NAMA=models.CharField(max_length=40)
+    TTL=models.CharField(max_length=40)
+    PHOTO=models.CharField(max_length=40)
+    EMAIL=models.CharField(max_length=40)
+    FAKULTAS=models.CharField(max_length=40)
+    PRODI=models.CharField(max_length=40)
+
+    def _str_(self):
+        return self.NIP
+
+class DOSEN(models.Model):
+    NIM=models.CharField(max_length=50)
+    NAMA=models.CharField(max_length=40)
+    TTL=models.CharField(max_length=40)
+    PHOTO=models.CharField(max_length=40)
+    EMAIL=models.CharField(max_length=40)
+    FAKULTAS=models.CharField(max_length=40)
+    PRODI=models.CharField(max_length=40)
+    ALAMAT=models.CharField(max_length=40)
+    
+    
+    def _str_(self):
+        return self.NIP
